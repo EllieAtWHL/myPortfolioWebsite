e758b30348e998f34daad18bd88bf37790184331
EllieAtWHL <eleanormatthewman@gmail.com>
2022-04-30 15:42:09 +0100

    more css improvements and undo button visible

--- regicide/regicide.js
+++ regicide/regicide.js
@@ -78,16 +78,18 @@ handSlots.forEach(slot => {
 
 attackButton.addEventListener('click', handlePlayerAttack)
 jesterButton.addEventListener('click', handleUseJester)
+undoButton.addEventListener('click', handleUndoMove)
 
 
 function startGame() {
     gameStart.style.display = 'none'
     playArea.style.display = 'grid'
-    attackButton.disabled = true
+    attackButton.style.visibility = 'hidden'
     onAttack = true
     chosenCards = undefined
     activeDeck = undefined
     discardDeck = undefined
+    clearDefenceMessage()
     updateJesterText()
     clearActiveDeck()
     updateDiscardPile()
@@ -169,7 +171,7 @@ function cardSelected(){
             alertBox('Illegal move')
         }
         else {
-            attackButton.disabled = false
+            attackButton.style.visibility = 'visible'
             moveSelectedCardToChosenCards(selectedCard)
             let slotToClear = event.path[1]
             renderSelectedCardMovement(slotToClear)
@@ -331,7 +333,7 @@ function playerAttack(suits){
         if (!activeDeck) activeDeck = new Deck(chosenCards.cards)
         else activeDeck = new Deck(activeDeck.cards.concat(chosenCards.cards))
         updateHealthText()
-        attackButton.disabled = true
+        attackButton.style.visibility = 'hidden'
         if(playerHand.numberOfCards === 0 && jestersRemaining === 0) alertBox(['Sorry, you lost'], true)
         else handleRoyalAttack(suits)
     }
@@ -415,7 +417,7 @@ function clearActiveDeck(){
     activeSlots.forEach(slot => {
         slot.innerHTML = ''
     })
-    attackButton.disabled = true
+    attackButton.style.visibility = 'hidden'
 }
 
 function updateDiscardPile(){
@@ -469,6 +471,10 @@ function updateJesterText(){
 
     let jesterMessage = document.getElementById('jester-message')
     jesterMessage.innerText = `Jesters left: ${jestersRemaining}`
+}
+
+function handleUndoMove(){
+    console.log('Undo!')
 }
 
 function wonGame(){
