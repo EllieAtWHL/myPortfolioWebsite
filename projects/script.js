d46130b485934b03ec6f2728b27be297e6206885
EllieAtWHL <eleanormatthewman@gmail.com>
2022-05-29 22:35:17 +0100

    projects desktop

---
+++ projects/script.js
@@ -0,0 +1,44 @@
+const buttonElements = document.querySelectorAll('.card')
+
+buttonElements.forEach(buttonElement => {
+    buttonElement.addEventListener('click', handleClick)
+})
+
+function handleClick(event){
+    
+    let button = event.currentTarget
+
+    let hover = button.querySelector('.card-desc')
+    hover.classList.toggle('closed')
+    hover.classList.toggle('open')
+
+    let links = button.querySelector('.links')
+    links.classList.toggle('closed')
+    links.classList.toggle('open')
+
+    let nextSibling = button.nextElementSibling
+    let prevSibling = button.previousElementSibling
+
+    while(nextSibling) {
+        let hover = nextSibling.querySelector('.card-desc')
+        hover.classList.remove('closed')
+        hover.classList.add('open')
+
+        let links = nextSibling.querySelector('.links')
+        links.classList.add('closed')
+        links.classList.remove('open')
+
+        nextSibling = nextSibling.nextElementSibling;
+    }
+    while(prevSibling) {
+        let hover = prevSibling.querySelector('.card-desc')
+        hover.classList.remove('closed')
+        hover.classList.add('open')
+
+        let links = prevSibling.querySelector('.links')
+        links.classList.add('closed')
+        links.classList.remove('open')
+
+        prevSibling = prevSibling.previousElementSibling;
+    }
+}
