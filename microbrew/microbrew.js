c34cfc862b0c118e9418d9066feb4191d673d9b3
EllieAtWHL <eleanormatthewman@gmail.com>
2023-02-18 13:45:46 +0000

    start game set up - backend

--- microbrew/microbrew.js
+++ microbrew/microbrew.js
@@ -1,6 +1,3 @@
-import { Customer_Deck } from "./scripts/customer.js";
-import { Recipe_Deck } from "./scripts/recipe.js";
-import { Reputation_Deck } from "./scripts/reputation.js";
 import { Game } from "./scripts/game.js";
 
 const MALT_COLOUR_MAP = {
@@ -17,46 +14,16 @@ const RECIPES = document.getElementById('recipes')
 
 PLAY_BUTTON.addEventListener('click', startGame)
 
+
 function startGame() {
-    console.log(`Game starting`)
-    
-    let game = new Game('Ellie', 'Dave');
+
+    let playerOneName = document.getElementById('playerOne').value || 'Player One';
+    let playerTwoName = document.getElementById('playerTwo').value || 'Player Two';
+    let game = new Game(playerOneName, playerTwoName);
 
     console.log(game)
 
-    game.displayCopper(game.playerOne);
-
     GAME_START.style.display = 'none'
     PLAY_AREA.style.display = 'inline-block'
-
-    //TODO: Select token to swap to hops
-
-    //TODO: Confirm swap
-
-    //TODO Switch to player two
-
-    //TODO: Select tokens to swap to hops
-
-    //TODO: Confirm swap
-
-    // let customers = new Customer_Deck();
-    // customers.shuffle();
-    // const customersText = document.createElement('p')
-    // customersText.innerText = JSON.stringify(customers)
-    // CUSTOMERS.append(customersText)
-    // let deal = new Customer_Deck(customers.deal())
     
-
-    // let recipes = new Recipe_Deck();
-    // recipes.shuffle()
-    // const recipesText = document.createElement('p')
-    // recipesText.innerText = JSON.stringify(recipes)
-    // RECIPES.append(recipesText)
-    // let chosen = new Recipe_Deck(recipes.deal())
-
-    // let reputations = new Reputation_Deck();
-    // console.log(reputations)
-
-    /*console.log(JSON.stringify(this.PLAYER_ONE))
-    console.log(JSON.stringify(this.PLAYER_TWO));*/
 }
--- microbrew/scripts/game.js
+++ microbrew/scripts/game.js
@@ -1,12 +1,9 @@
 import Modal from "../../scripts/modal.js";
+import { TOKENS_MAP, Token_Pot, Token } from "./token.js";
+import { Customer_Deck } from "./customer.js";
+import { Recipe_Deck } from "./recipe.js";
+import { Reputation_Deck } from "./reputation.js";
 
-const TOKENS_MAP =
-    [
-        { 'type': 'yellow', 'count': 16, 'image': 'img/hexagon_yellow.svg' },
-        { 'type': 'orange', 'count': 16, 'image': 'img/hexagon_orange.svg' },
-        { 'type': 'brown', 'count': 16, 'image': 'img/hexagon_brown.svg' },
-        { 'type': 'hops', 'count': 6, 'image': 'img/hexagon_green.svg' }
-    ]
 
 const PLAYERS = 2;
 const TOKENS_PER_COLUMN = 4;
@@ -16,9 +13,25 @@ let currentGame;
 export class Game {
     constructor(name1, name2) {
         currentGame = this;
-        this.pot = new Token_Pot();
-        this.pot.shuffle();
 
+        this.tokenPot = this.newGameTokenPot();
+        this.newGamePlayers(name1, name2);
+        this.newGameCustomers();
+        this.newGameRecipes();
+        this.newGameReputation();
+
+        this.updateDisplay();
+
+        this.addHops();
+    }
+
+    newGameTokenPot(){
+        let pot = new Token_Pot();
+        pot.shuffle();
+        return pot;
+    }
+
+    newGamePlayers(name1, name2){
         this.playerOne = new Player(name1, 1);
         this.playerTwo = new Player(name2, 2);
 
@@ -27,8 +40,41 @@ export class Game {
 
         this.playerOne.copper = this.fillPlayerCopper();
         this.playerTwo.copper = this.fillPlayerCopper();
+    }
 
-        this.addHops();
+    newGameCustomers(){
+        this.customerDeck = new Customer_Deck();
+        this.customerDeck.shuffle();
+
+        this.playerOne.loyalCustomerDeck = new Customer_Deck([this.customerDeck.deal()])
+        this.playerTwo.loyalCustomerDeck = new Customer_Deck([this.customerDeck.deal()])
+
+        this.thirstyCustomers = new Customer_Deck([this.customerDeck.deal()]);
+        this.thirstyCustomers = new Customer_Deck(this.thirstyCustomers.deck.concat(this.customerDeck.deal()));
+    }
+
+    newGameRecipes(){
+        this.recipeDeck = new Recipe_Deck();
+        this.recipeDeck.shuffle();
+
+        this.playerOne.recipeHand = new Recipe_Deck([this.recipeDeck.deal()]);
+        this.playerTwo.recipeHand = new Recipe_Deck([this.recipeDeck.deal()]);
+
+        this.recipeBoard = new Recipe_Deck([this.recipeDeck.deal()]);
+        this.recipeBoard = new Recipe_Deck(this.recipeBoard.deck.concat(this.recipeDeck.deal()));
+        this.recipeBoard = new Recipe_Deck(this.recipeBoard.deck.concat(this.recipeDeck.deal()));
+    }
+
+    newGameReputation(){
+        this.reputationDeck = new Reputation_Deck();
+        this.reputationDeck.shuffle();
+
+        this.publicReputation = new Reputation_Deck([this.reputationDeck.deal()]);
+
+        this.playerOne.reputation = new Reputation_Deck([this.reputationDeck.deal()]);
+        this.playerOne.reputation = new Reputation_Deck(this.playerOne.reputation.deck.concat(this.reputationDeck.deal()));
+        this.playerTwo.reputation = new Reputation_Deck([this.reputationDeck.deal()]);
+        this.playerTwo.reputation = new Reputation_Deck(this.playerTwo.reputation.deck.concat(this.reputationDeck.deal()));
     }
 
     fillPlayerCopper() {
@@ -41,11 +87,10 @@ export class Game {
         return playerCopper;
     }
 
-
     fillCol() {
         let row = []
         for (let i = 0; i < TOKENS_PER_COLUMN; i++) {
-            row.push(this.pot.dealToken());
+            row.push(this.tokenPot.dealToken());
         }
         return row;
     }
@@ -57,13 +102,47 @@ export class Game {
             let hopsToken = new Token('hops', `hops${i}`);
             hopsTokens.push(hopsToken);
         }
-        this.pot = new Token_Pot(this.pot.pot.concat(hopsTokens));
-        this.pot.shuffle();
+        this.tokenPot = new Token_Pot(this.tokenPot.pot.concat(hopsTokens));
+        this.tokenPot.shuffle();
 
     }
 
+    updateDisplay(){
+        console.log('Display public info');//TODO: display public information
+        this.displayCustomers(currentGame.turn);
+        this.displayRecipes(currentGame.turn);
+        this.displayReputation(currentGame.turn);
+        this.displayCopper(currentGame.turn);
+    }
+
+    displayCustomers(player){
+        console.log('Display customers'); //TODO:
+        console.log(player.loyalCustomerDeck);
+    }
+    displayRecipes(player){
+        console.log('Display recipes'); //TODO:
+        console.log(player.recipeHand);
+    }
+    displayReputation(player){
+        console.log('Display reputation'); //TODO:
+        console.log(player.reputation);
+    }
+
     displayCopper(player) {
-        displayCopper(player);
+        let playerIndicator = document.getElementById('player-name');
+
+        playerIndicator.innerHTML = null;
+        playerIndicator.innerText = `${player.name} - £${player.cash}`;
+
+        player.copper.content.forEach((column, colIndex) => {
+            column.forEach((row, rowIndex) => {
+                let tokenDiv = document.querySelector(`[data-row="${(rowIndex + 1).toString()}"][data-col="${(colIndex + 1).toString()}"]`)
+                tokenDiv.innerHTML = null;
+                const tokenImage = fillTokenSlot(row.type, row.id, player);
+                tokenImage.addEventListener('click', handleClick);
+                tokenDiv.appendChild(tokenImage);
+            })
+        })
     }
 
     handleClick(event) {
@@ -75,6 +154,7 @@ export class Player {
     constructor(name, number) {
         this.name = name;
         this.number = number;
+        this.cash = 0;
     }
 }
 
@@ -92,65 +172,6 @@ export class Copper {
     discardToken(column, row, tokenToReplace){
         this.content[column][row] = tokenToReplace;
     }
-}
-
-class Token_Pot {
-    constructor(pot = startingPot()) {
-        this.pot = pot;
-    }
-
-    get numberOfTokens() {
-        return this.pot.length;
-    }
-
-    shuffle() {
-        for (let i = this.numberOfTokens - 1; i > 0; i--) {
-            const newIndex = Math.floor(Math.random() * (i + 1))
-            const oldValue = this.pot[newIndex]
-            this.pot[newIndex] = this.pot[i]
-            this.pot[i] = oldValue
-        }
-    }
-
-    dealToken() {
-        return this.pot.shift();
-    }
-}
-
-class Token {
-    constructor(type, id) {
-        this.type = type;
-        this.id = id;
-    }
-}
-
-function startingPot() {
-    let pot = [];
-    TOKENS_MAP.forEach(type => {
-        if (type.type === 'hops') return;
-        for (let i = 0; i < type.count; i++) {
-            pot.push(new Token(type.type, `${type.type + i}`));
-        }
-    })
-    return pot;
-}
-
-function displayCopper(player) {
-
-    let playerIndicator = document.getElementById('player-name');
-
-    playerIndicator.innerHTML = null;
-    playerIndicator.innerText = player.name;
-
-    player.copper.content.forEach((column, colIndex) => {
-        column.forEach((row, rowIndex) => {
-            let tokenDiv = document.querySelector(`[data-row="${(rowIndex + 1).toString()}"][data-col="${(colIndex + 1).toString()}"]`)
-            tokenDiv.innerHTML = null;
-            const tokenImage = fillTokenSlot(row.type, row.id, player);
-            tokenImage.addEventListener('click', handleClick);
-            tokenDiv.appendChild(tokenImage);
-        })
-    })
 }
 
 function fillTokenSlot(type, id, player) {
@@ -182,7 +203,7 @@ function handleClick(event){
     switch(currentGame.status){
         case 'Start': 
             insertHop(clickedToken);
-            displayCopper(currentGame.turn);
+            currentGame.displayCopper(currentGame.turn);
             let confirmed = confirmHopSwitch();
             if(confirmed){
                 switch(clickedToken.dataset.player) {
@@ -196,7 +217,7 @@ function handleClick(event){
                     default:
                         console.log('There\'s something happening here');
                 }
-                displayCopper(currentGame.turn);
+                currentGame.displayCopper(currentGame.turn);
             } else {
                 console.log('TODO: Undo') // TODO:
             }
